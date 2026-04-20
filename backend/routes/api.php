f226cbe888a25b0ff8e2702e21e554eee84c1c00
alliance0404 <alliance940404@gmail.com>
2026-04-21 00:55:35 +0800

    fix backend little

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,11 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
-// Route::middleware('auth:sanctum')->group(function () {
-//   Route::get('/me', [UserController::class, 'me']);
-//   Route::middleware('authAdmin')->get('/admin', [UserController::class, 'admin']);
-//   Route::middleware('authManagerOrAdmin')->get('/manager-or-admin', [UserController::class, 'managerOrAdmin']);
-// });
+Route::middleware('auth:sanctum')->group(function () {
+  Route::get('/me', [UserController::class, 'me']);
+  Route::middleware('authAdmin')->get('/admin', [UserController::class, 'admin']);
+  Route::middleware('authManagerOrAdmin')->get('/manager-or-admin', [UserController::class, 'managerOrAdmin']);
+});
 Route::post('/fake-saml-login', [UserController::class, 'fakeSamlLogin']);
-Route::get('/me', [UserController::class, 'me']);
 
