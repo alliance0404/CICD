4d77534bdab55650a5f930e7691b93df6e1bc232
alliance0404 <alliance940404@gmail.com>
2026-04-21 00:23:39 +0800

    fix backend

--- backend/app/Http/Controllers/Api/UserController.php
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 {
     public function me(Request $request)
     {
-        return response()->json(['aaa' => 'bbb'])
+        return response()->json(['aaa' => 'bbb']);
         $user = $request->user(); // authenticated user
         return response()->json([
             'id' => $user->id,
